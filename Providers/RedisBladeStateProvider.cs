d20dd70c52ee8c9c65e468d941236c7143bcaa42
Tom Rooker <2650021+LordDurus@users.noreply.github.com>
2025-09-01 14:58:35 -0400

    Removed an un=needed space

--- Providers/RedisBladeStateProvider.cs
+++ Providers/RedisBladeStateProvider.cs
@@ -7,32 +7,31 @@ namespace BladeState.Providers;
 
 public class RedisBladeStateProvider<T> : BladeStateProvider<T> where T : class, new()
 {
-    private readonly IDatabase _redis;
-    private readonly string _key;
+	private readonly IDatabase _redis;
+	private readonly string _key;
 
-    public RedisBladeStateProvider(IConnectionMultiplexer redis, string key = "BladeState")
-    {
+	public RedisBladeStateProvider(IConnectionMultiplexer redis, string key = "BladeState")
+	{
+		_redis = redis.GetDatabase();
+		_key = $"{key}-{Profile.Id}";
+	}
 
-        _redis = redis.GetDatabase();
-        _key = $"{key}-{Profile.Id}";
-    }
+	public override async Task<T> LoadStateAsync()
+	{
+		var value = await _redis.StringGetAsync(_key);
+		if (value.IsNullOrEmpty) return new T();
 
-    public override async Task<T> LoadStateAsync()
-    {
-        var value = await _redis.StringGetAsync(_key);
-        if (value.IsNullOrEmpty) return new T();
+		return JsonSerializer.Deserialize<T>(value!);
+	}
 
-        return JsonSerializer.Deserialize<T>(value!);
-    }
+	public override async Task SaveStateAsync(T state)
+	{
+		var json = JsonSerializer.Serialize(state);
+		await _redis.StringSetAsync(_key, json);
+	}
 
-    public override async Task SaveStateAsync(T state)
-    {
-        var json = JsonSerializer.Serialize(state);
-        await _redis.StringSetAsync(_key, json);
-    }
-
-    public override async Task ClearStateAsync()
-    {
-        await _redis.KeyDeleteAsync(_key);
-    }
+	public override async Task ClearStateAsync()
+	{
+		await _redis.KeyDeleteAsync(_key);
+	}
 }
