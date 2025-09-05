881ab47a2450cc6d2d590fcc7e6cf45311e645c6
Tom Rooker <2650021+LordDurus@users.noreply.github.com>
2025-09-05 14:17:33 -0400

    Merged. And added memory cache provider

--- Profile.cs
+++ Profile.cs
@@ -5,4 +5,5 @@ public class Profile
 {
 	public string Id { get; set; } = Guid.NewGuid().ToString();
 	public string IpAddress { get; set; } = string.Empty;
+	public TimeSpan SessionTimeout { get; set; } = TimeSpan.FromHours(12);
 }
